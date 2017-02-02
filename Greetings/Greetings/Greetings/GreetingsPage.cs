ac42c6d4f9247f3685ae2de8a6395c3fc4765852
Tiago Versuti <tiagoversuti@gmail.com>
2017-02-02 10:21:46 -0200

    Módulo 1: Introdução ao desenvolvimento mobile     Lição 4: Portabilidade de código .Net         Laboratório: Compartilhando código entre projetos multiplataforma

---
+++ Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            Padding = Device.OnPlatform<Thickness>(
+                new Thickness(0, 20, 0, 0), // iOS
+                new Thickness(0), // Android
+                new Thickness(0) // WinPhone
+                );
+
+            var MyLabel = new Label();
+            MyLabel.Text = "Greetings, Xamarin.Forms!";
+            this.Content = MyLabel;
+
+            //MyLabel.HorizontalOptions = LayoutOptions.Center;
+            //MyLabel.VerticalOptions = LayoutOptions.Center;
+
+            MyLabel.HorizontalTextAlignment = TextAlignment.Center;
+            MyLabel.VerticalTextAlignment = TextAlignment.Center;
+        }
+    }
+}
