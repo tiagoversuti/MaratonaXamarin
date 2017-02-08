521fa2f35c57f3771316a28857db35b10bf47356
Tiago Versuti <tiagoversuti@gmail.com>
2017-02-08 15:38:52 -0200

    Módulo 2: Xamarin.Android     Lição 1: Introdução à interface Gráfica em Xamarin         Gerando um aplicativo com interface gráfica do tipo Relative

---
+++ AbsoluteLayout/AbsoluteLayout/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AbsoluteLayout
+{
+    [Activity(Label = "AbsoluteLayout", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            Button btnConverter = FindViewById<Button>(Resource.Id.btnconverter);
+            EditText txtDolares = FindViewById<EditText>(Resource.Id.txtdolares);
+            EditText txtPesos = FindViewById<EditText>(Resource.Id.txtpesos);
+            double pesos, dolares;
+            btnConverter.Click += delegate
+            {
+                try
+                {
+                    dolares = double.Parse(txtDolares.Text);
+                    pesos = dolares * 19.5;
+                    txtPesos.Text = pesos.ToString();
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+                }
+            };
+        }
+    }
+}
+
